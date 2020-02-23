30521eafa81101ed9c7f45c713d8faf012e7479f
Fajar van Megen <3041538+fajarvm@users.noreply.github.com>
2020-02-23 05:38:48 +0100

    Updated generator script to be more configurable. Also updated the package name and version inside openapi config file

--- generate.sh
+++ generate.sh
@@ -1,16 +1,29 @@
 #!/usr/bin/env bash
+#######################
+####### configs #######
+#######################
+## Dart generator: dart | dart-dio | dart-jaguar
+DART_GENERATOR="dart"
 
 ## The custom template directory for Dart2
-TEMPLATE_DIR="./dart2-template"
+TEMPLATE_DIR="dart2-template"
 
 ## Source folder for generated code as defined in the 'openapi-config.yml'
-SOURCE_FOLDER="./lib/src/sdk/infrastructure"
+SOURCE_FOLDER="lib/src/sdk/infrastructure"
 
+## OpenAPI custom configuration file
+OPENAPI_CONFIG_FILE="openapi-config.yml"
+
+## OpenAPI definition file name
+OPENAPI_DEFINITION_FILE="openapi3.yml"
+
+#########################
+####### operation #######
+#########################
 ## Download NEM2 OpenAPI definition
-## NEM2 OpenAPI v3: https://github.com/nemtech/nem2-openapi/
-curl -O https://nemtech.github.io/nem2-openapi/openapi3.yml
-
-OPENAPI_DEFINITION_FILE="./openapi3.yml"
+## NEM2 OpenAPI v3: https://github.com/nemtech/symbol-openapi/
+curl -o $OPENAPI_DEFINITION_FILE https://nemtech.github.io/symbol-openapi/openapi3.yml
+#curl -o $OPENAPI_DEFINITION_FILE https://raw.githubusercontent.com/NEMStudios/nem2-open-api-generator/master/openapi3-any-of-patch.yml
 
 ## Install openapi-generator CLI
 ## Dart generator issue: https://github.com/OpenAPITools/openapi-generator/pull/3656
@@ -32,14 +45,17 @@ brew list openapi-generator || brew install openapi-generator
 ## Param: -t path/to/custom_template_dir
 ## Uses the custom template when generating the code.
 openapi-generator generate \
- -g dart-dio \
+ -g $DART_GENERATOR \
  -i $OPENAPI_DEFINITION_FILE \
- -c openapi-config.yml \
+ -c $OPENAPI_CONFIG_FILE \
  --generate-alias-as-model \
  --enable-post-process-file
 # -t $TEMPLATE_DIR
 
+#######################
+###### clean-up #######
+#######################
 ## Move generated files to the correct source folder. For some reasons, the generator creates
 ## an extra 'lib' folder and puts all of the generated code in there.
-OUTPUT_LIB_FOLDER="$SOURCE_FOLDER/lib"
-mv $OUTPUT_LIB_FOLDER/* $SOURCE_FOLDER && rmdir $OUTPUT_LIB_FOLDER
+# OUTPUT_LIB_FOLDER="$SOURCE_FOLDER/lib"
+# mv $OUTPUT_LIB_FOLDER/* $SOURCE_FOLDER && rmdir $OUTPUT_LIB_FOLDER
